bcc1d8b6a54c1949bc45e7b1f3cf0f4220d43f8b
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-12-08 22:26:09 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -32,26 +32,26 @@ def stream_markup_timer(_, chat_id, played, dur):
     duration_sec = time_to_seconds(dur)
     percentage = (played_sec / duration_sec) * 100
     umm = math.floor(percentage)
-    if 0 < umm <= 10:
-        bar = "◉—————————"
-    elif 10 < umm < 20:
-        bar = "—◉————————"
-    elif 20 <= umm < 30:
-        bar = "——◉———————"
-    elif 30 <= umm < 40:
-        bar = "———◉——————"
-    elif 40 <= umm < 50:
-        bar = "————◉—————"
-    elif 50 <= umm < 60:
-        bar = "—————◉————"
-    elif 60 <= umm < 70:
-        bar = "——————◉———"
-    elif 70 <= umm < 80:
-        bar = "———————◉——"
-    elif 80 <= umm < 95:
-        bar = "————————◉—"
+    if 0 < anon <= 10:
+        bar = "♪━━━━━━━━━"
+    elif 10 < anon < 20:
+        bar = "━♪━━━━━━━━"
+    elif 20 <= anon < 30:
+        bar = "━━♪━━━━━━━"
+    elif 30 <= anon < 40:
+        bar = "━━━♪━━━━━━"
+    elif 40 <= anon < 50:
+        bar = "━━━━♪━━━━━"
+    elif 50 <= anon < 60:
+        bar = "━━━━━♪━━━━"
+    elif 60 <= anon < 70:
+        bar = "━━━━━━♪━━━"
+    elif 70 <= anon < 80:
+        bar = "━━━━━━━♪━━"
+    elif 80 <= anon < 95:
+        bar = "━━━━━━━━♪━"
     else:
-        bar = "—————————◉"
+        bar = "━━━━━━━━━♪"
     buttons = [
         [
             InlineKeyboardButton(text="▷", callback_data=f"ADMIN Resume|{chat_id}"),
