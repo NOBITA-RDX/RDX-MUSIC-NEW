d7e25459d3d7b346d6037a08cbce6e3001f2c309
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-12-09 16:58:44 +0530

    Update start.py

--- AnonXMusic/utils/inline/start.py
+++ AnonXMusic/utils/inline/start.py
@@ -26,12 +26,12 @@ def private_panel(_):
         ],
         [InlineKeyboardButton(text=_["S_B_4"], callback_data="settings_back_helper")],
         [
-            InlineKeyboardButton(text=_["S_B_5"], url="tg://user?id=1777270311"),
+            InlineKeyboardButton(text=_["S_B_5"], url="user_id=1777270311"),
             InlineKeyboardButton(text=_["S_B_2"], url="https://t.me/+MQn7rXz1LeViZmJl"),
         ],
         [
-            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/+m4oVCt2zFhYyMTdl"),
-            InlineKeyboardButton(text=_["S_B_7"], url=config.UPSTREAM_REPO),
+            InlineKeyboardButton(text=_["S_B_6"], url="https://t.me/+PtOLQT04ocMzOTJl"),
+            InlineKeyboardButton(text=_["S_B_7"], url="https://t.me/+m4oVCt2zFhYyMTdl"),
         ],
     ]
     return buttons
