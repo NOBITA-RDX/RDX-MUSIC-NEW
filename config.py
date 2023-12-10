bda4afd24f295efec954246a8b145dc5352afb6a
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-12-10 16:29:12 +0530

    Update config.py

--- config.py
+++ config.py
@@ -78,10 +78,10 @@ confirmer = {}
 
 
 START_IMG_URL = getenv(
-    "START_IMG_URL", "https://graph.org/file/2d497c2e7319cc7a0cfd4.jpg"
+    "START_IMG_URL", "https://graph.org/file/986c30c92c6e1f2759e53.jpg"
 )
 PING_IMG_URL = getenv(
-    "PING_IMG_URL", "https://graph.org/file/90d430c34e3892fd754dd.jpg"
+    "PING_IMG_URL", "https://graph.org/file/a06db6687a8edf81b91d6.jpg"
 )
 PLAYLIST_IMG_URL = "https://te.legra.ph/file/4ec5ae4381dffb039b4ef.jpg"
 STATS_IMG_URL = "https://te.legra.ph/file/e906c2def5afe8a9b9120.jpg"
