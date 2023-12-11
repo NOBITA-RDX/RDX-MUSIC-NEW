b7243f66026cf26e142399d3a0297bf764ad18a3
NOBITA-RDX <141653116+NOBITA-RDX@users.noreply.github.com>
2023-12-12 00:32:14 +0530

    Update play.py

--- AnonXMusic/utils/inline/play.py
+++ AnonXMusic/utils/inline/play.py
@@ -96,6 +96,12 @@ def stream_markup(_, chat_id):
             InlineKeyboardButton(text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"),
             InlineKeyboardButton(text="▢", callback_data=f"ADMIN Stop|{chat_id}"),
         ],
+        [
+            InlineKeyboardButton(
+                text=f"{played} {bar} {dur}",
+                callback_data="GetTimer",
+            ),
+        ],
         [InlineKeyboardButton(
                 text="˹sᴜᴘᴘ๏ʀᴛ˼", url="https://t.me/+WRmWApnCkrJmOGFl"
             ),
